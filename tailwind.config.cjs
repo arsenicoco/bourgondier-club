1a0990778bd4fced34426924687b48c62d5936de
Arseniy Korobchenko <ars@hey.com>
2025-09-04 12:36:00 +0200

    content updates; fixed dark vs light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -11,6 +12,12 @@ module.exports = {
         background: "#FDFDFD",
         textColor: "#333333",
         borderColor: "#EAEAEA",
+        dark: {
+          primary: "#E57373",
+          background: "#121212",
+          textColor: "#e0e0e0",
+          borderColor: "#424242",
+        },
       },
       fontFamily: {
         sans: ["DM Mono", ...defaultTheme.fontFamily.sans],
